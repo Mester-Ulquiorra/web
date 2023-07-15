32da7145bd0562fdcc3faa78ba900d2365bf0ca8
noClaps <github@zerolimits.dev>
2023-07-15 22:08:37 +0100

    Add user avatar and user tag in header

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,8 +1,20 @@
-import { getUserData } from "./userData";
+import { getUserData } from "./userData"
 
-let userData
 const apiUrl = new URL(`https://ucp.mester.info/api/user?test=true`)
+const userData = await getUserData(apiUrl)
 
-if (!userData) {
-    userData = await getUserData(apiUrl)
+if (userData) {
+    // Setting the user avatar
+    const userAvatar = document.querySelector("#avatar") as HTMLImageElement
+    userAvatar.src = `https://media.discordapp.net/avatars/${userData?.userId}/${userData?.avatar}.png`
+
+    // Setting the user tag
+    const username = document.querySelector("#username") as HTMLParagraphElement
+    username.innerHTML = userData?.userTag ?? ""
+
+    const loginButton = document.querySelector("#login") as HTMLFormElement
+    loginButton.style.display = "none"
+
+    const userInfo = document.querySelector("#user-info") as HTMLDivElement
+    userInfo.style.display = "inline-flex"
 }
