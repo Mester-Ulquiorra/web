84858d4ef77ca92449a49f78a7ffd0411b95135b
noClaps <github@zerolimits.dev>
2023-07-16 07:05:11 +0100

    Optimise static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ucp.mester.info"
+    build: {
+        inlineStylesheets: 'auto'
+    },
+    compressHTML: true,
+    scopedStyleStrategy: 'class',
+    site: "https://ucp.mester.info",
 });
