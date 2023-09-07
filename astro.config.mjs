6fa21711a06659309c94d1b788efdbbd9f7797a7
noClaps <github@zerolimits.dev>
2023-09-08 01:28:51 +0800

    Fix failing build
    Why does Cloudflare not use a Node environment? Seems pretty silly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ export default defineConfig({
   site: "https://ucp.mester.info",
   output: "server",
   adapter: cloudflare(),
-  image: { service: { entrypoint: "astro/assets/services/noop" } }
+  image: { service: { entrypoint: "astro/assets/services/noop" } },
+  vite: {
+    resolve: {
+      alias: {
+        "svgo": import.meta.env.PROD ? "svgo/dist/svgo.browser.js" : "svgo"
+      }
+    }
+  }
 });
