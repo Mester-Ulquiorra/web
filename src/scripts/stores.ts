920371a3b2f2c083aff8543cc08742c89c098b1e
Mester Sosuke <MesterMan03+github@proton.me>
2023-09-08 19:50:12 +0200

    Update stores.ts
    revert an explicit check to use the ! operator

--- src/scripts/stores.ts
+++ src/scripts/stores.ts
@@ -5,12 +5,8 @@ export const $toastText = atom("");
 $toastText.listen((text) => {
   if (text === "") return;
 
-  // it's generally not a good idea to use the ! operator, it's better to explicitely check if the element exists
-  const toast = document.querySelector<HTMLDivElement>(".toast");
-  if(!toast) {
-    console.error("Toast element not found");
-    return;
-  }
+  // space dust effect
+  const toast = document.querySelector<HTMLDivElement>(".toast")!;
 
   toast.innerHTML = text;
 
