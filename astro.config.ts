838e04d06e457406f7c4a4b18403b748d51d527c
MesterMan03 <MesterMan03+github@proton.me>
2023-12-05 22:57:28 +0100

    update to vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ucp.mester.info",
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
